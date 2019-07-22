c1144d2e17c02787d3c2f5572d7a92f7ad30758a
Chris Trevino <darthtrevino@gmail.com>
2019-07-22 15:35:06 -0700

    Add initializeIcons call

--- src/index.tsx
+++ src/index.tsx
@@ -2,5 +2,8 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
 import App from './components/App'
+import { initializeIcons } from '@uifabric/icons'
+
+initializeIcons()
 
 ReactDOM.render(<App />, document.getElementById('root'))
