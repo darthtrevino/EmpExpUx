f619110b7c9f96e20c5a42e0fdb21322f0bbd746
Brian Garland <bgarland@bpcs.com>
2019-07-22 17:00:31 -0700

    added example of scss modules

--- src/components/pages/Home/index.tsx
+++ src/components/pages/Home/index.tsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import { Page } from '../../Page'
+import styles from './Home.module.scss'
 
 export const Home: React.FC = () => {
 	return (
 		<Page name="Home">
+			<div className={styles.myDiv}>This should be red</div>
 			<div>content</div>
 		</Page>
 	)
