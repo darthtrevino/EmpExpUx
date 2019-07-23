008331179e68f9cbd62a20e223bee8189384f421
Chris Trevino <darthtrevino@gmail.com>
2019-07-22 18:55:32 -0700

    Remove unused imports

--- src/components/pages/FindExperts/index.tsx
+++ src/components/pages/FindExperts/index.tsx
@@ -1,10 +1,5 @@
 import React from 'react'
-import styled from 'styled-components'
 import { Page } from '../../Page'
-import { useProjects } from '../../../hooks/useProjects'
-import { useSkills } from '../../../hooks/useSkills'
-import { useTopics } from '../../../hooks/useTopics'
-import { CategoryFilter } from '../../Filters/CategoryFilter'
 import { FilterPane } from './FilterPane'
 
 export const FindExperts: React.FC = () => {
