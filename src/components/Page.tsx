df783f3b9d1bc4ef80e27d59e536065151900c3f
Chris Trevino <darthtrevino@gmail.com>
2019-07-23 15:42:01 -0700

    use react-helmet for setting the page title when the page changes

--- src/components/Page.tsx
+++ src/components/Page.tsx
@@ -1,11 +1,15 @@
 import React, { memo } from 'react'
 import styles from './Page.module.scss'
+import { Helmet } from 'react-helmet'
 
 export interface PageProps {
 	name: string
 }
 export const Page: React.FC<PageProps> = memo(({ name, children }) => (
 	<div className={styles.pageContainer}>
+		<Helmet>
+			<title>EmpExp - {name}</title>
+		</Helmet>
 		<div className={styles.contentCnt}>{children}</div>
 	</div>
 ))
