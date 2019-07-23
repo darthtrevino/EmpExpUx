913787000eef900b79ea7b5510aed234bed00742
Chris Trevino <darthtrevino@gmail.com>
2019-07-23 14:39:06 -0700

    push header to new component, load current user in header

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -5,26 +5,15 @@ import { Routes } from './Routes'
 import { Navigation } from './Navigation'
 import store from '../../state'
 import styles from './App.module.scss'
+import { Header } from './Header'
 import { BrowserRouter } from 'react-router-dom'
 
 export const App: React.FC = memo(() => {
-	const titleText = 'Employee Experience'
 	return (
 		<Provider store={store}>
 			<Fabric>
 				<div className={styles.appContainer}>
-					<div className={styles.header}>
-						<div className={styles.topBar}>
-							<div className={styles.brandmark}>
-								<img
-									src={require('../../images/brandmark_white.png')}
-									role="presentation"
-									alt={`${titleText} Templates Logo`}
-								/>
-							</div>
-							<div className={styles.title}>{titleText}</div>
-						</div>
-					</div>
+					<Header />
 					<BrowserRouter>
 						<div className={styles.contentContainer}>
 							<div className={styles.sidePanel}>
---
+++ src/components/App/Header.tsx
@@ -0,0 +1,22 @@
+import React, { memo } from 'react'
+import styles from './App.module.scss'
+import { useCurrentUser } from '../../hooks/useCurrentUser'
+
+const brandMark = require('../../images/brandmark_white.png')
+const titleText = 'Employee Experience'
+const imgAlt = `${titleText} Templates Logo`
+
+export const Header: React.FC = memo(() => {
+	const currentUser = useCurrentUser()
+	return (
+		<div className={styles.header}>
+			<div className={styles.topBar}>
+				<div className={styles.brandmark}>
+					<img src={brandMark} role="presentation" alt={imgAlt} />
+				</div>
+				<div className={styles.title}>{titleText}</div>
+				<div className={styles.currentUser}>{currentUser || ''}</div>
+			</div>
+		</div>
+	)
+})
