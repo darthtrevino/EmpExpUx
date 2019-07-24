1d1670c76c4455538e8add7d72919f80a9b7cfb8
Chris Trevino <darthtrevino@gmail.com>
2019-07-24 15:00:47 -0700

    wire in # kudos from services

--- src/components/Header/UserDetail.tsx
+++ src/components/Header/UserDetail.tsx
@@ -3,9 +3,13 @@ import styles from './UserDetail.module.scss'
 import { Persona, PersonaSize } from 'office-ui-fabric-react'
 import { useCurrentUser } from '../../hooks/useCurrentUser'
 import { Medal } from './Medal'
+import { useSelector } from 'react-redux'
+import { State } from '../../state/reducers'
 
 export const UserDetail: React.FC = memo(() => {
 	const currentUser = useCurrentUser()
+	const numKudos = useSelector((state: State) => state.kudos.toMe.length)
+
 	return !!currentUser ? (
 		<div className={styles.userDetail}>
 			<Persona
@@ -13,7 +17,7 @@ export const UserDetail: React.FC = memo(() => {
 				size={PersonaSize.size32}
 				text={currentUser.email}
 			/>
-			<Medal metric={currentUser.email.length} name="Kudos" />
+			<Medal metric={numKudos} name="Kudos" />
 			<Medal metric={currentUser.rewardPoints} name="Points" />
 		</div>
 	) : null
