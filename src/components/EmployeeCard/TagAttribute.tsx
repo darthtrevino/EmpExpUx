cce918e5c5118324e1a217189e882920bb026b6f
Chris Trevino <darthtrevino@gmail.com>
2019-07-24 13:30:25 -0700

    Update TagAttribute to use tags

--- src/components/EmployeeCard/TagAttribute.tsx
+++ src/components/EmployeeCard/TagAttribute.tsx
@@ -1,16 +1,39 @@
 import React, { memo } from 'react'
+import { TagItem, ITagItemStyles } from 'office-ui-fabric-react'
 import styles from './Attributes.module.scss'
 
 export interface TagAttributeProps {
 	name: string
 	value: string[]
 }
-
 export const TagAttribute: React.FC<TagAttributeProps> = memo(
 	({ name, value }) => (
 		<div className={styles.attributeContainer}>
 			<div className={styles.attributeName}>{name}</div>
-			<div className={styles.attributeValue}>{value.join(', ')}</div>
+			<div className={styles.attributeValue}>
+				{value.map((v, index) => (
+					<TagItem
+						styles={tagStyle}
+						key={v}
+						index={index}
+						item={itemForValue(v)}
+					>
+						{v}
+					</TagItem>
+				))}
+			</div>
 		</div>
 	),
 )
+
+const tagStyle: ITagItemStyles = {
+	close: {
+		display: 'none',
+	},
+	root: {
+		display: 'inline',
+	},
+	text: {},
+}
+
+const itemForValue = (input: string) => ({ key: input, name: input })
