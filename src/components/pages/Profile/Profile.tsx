e7e0810dd3657b79e98a6b168b3eadbfc0ee54ff
Chris Trevino <darthtrevino@gmail.com>
2019-07-24 10:51:57 -0700

    make incoming requests pane expand/collapsible

--- src/components/pages/Profile/Profile.tsx
+++ src/components/pages/Profile/Profile.tsx
@@ -1,25 +1,41 @@
-import React, { memo } from 'react'
+import React, { memo, useState, useCallback } from 'react'
+import posed from 'react-pose'
 import { Page } from '../../Page'
 import { useSelector } from 'react-redux'
 import { State } from '../../../state/reducers'
 import styles from './Profile.module.scss'
 import { IncomingConnectionRequest } from './IncomingConnectionRequest'
+import { Icon } from 'office-ui-fabric-react'
 
 export const Profile: React.FC = memo(() => {
+	const [incomingExpanded, setIncomingExpanded] = useState(true)
 	const requestsMadeByMe = useSelector((s: State) => s.expertConnections.byMe)
 	const requestsMadeToMe = useSelector((s: State) => s.expertConnections.byMe)
+	const toggleIncomingExpanded = useCallback(
+		() => setIncomingExpanded(!incomingExpanded),
+		[incomingExpanded],
+	)
 	return (
 		<Page name="My Expertise">
 			<div className={styles.page}>
 				<div className={styles.gutter} />
 				<div className={styles.content}>
 					<div className={styles.requestSection}>
-						<div className={styles.requestSectionTitle}>Incoming Requests</div>
-						<div>
+						<div className={styles.requestSectionHeader}>
+							<div className={styles.requestSectionTitle}>
+								Incoming Requests
+							</div>
+							<Icon
+								className={styles.headerIcon}
+								iconName={incomingExpanded ? 'ChevronDown' : 'ChevronUp'}
+								onClick={toggleIncomingExpanded}
+							/>
+						</div>
+						<RequestSection pose={incomingExpanded ? 'expanded' : 'collapsed'}>
 							{requestsMadeToMe.map(r => (
 								<IncomingConnectionRequest key={r.id} connection={r} />
 							))}
-						</div>
+						</RequestSection>
 					</div>
 					<div className={styles.requestSection}>
 						<div className={styles.requestSectionTitle}>Outgoing Requests</div>
@@ -34,4 +50,9 @@ export const Profile: React.FC = memo(() => {
 			</div>
 		</Page>
 	)
+})
+
+const RequestSection = posed.div({
+	expanded: { height: 'auto', opacity: 1, zIndex: -1 },
+	collapsed: { height: '1px', opacity: 0, zIndex: 'auto' },
 })
