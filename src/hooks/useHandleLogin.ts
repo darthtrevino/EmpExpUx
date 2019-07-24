96998e9a166589bb41523d123a8a8713e9724871
Chris Trevino <darthtrevino@gmail.com>
2019-07-23 23:55:45 -0700

    load connection requests when the user changes

--- src/hooks/useHandleLogin.ts
+++ src/hooks/useHandleLogin.ts
@@ -1,7 +1,11 @@
 import { useCallback } from 'react'
 import { setCurrentUser } from '../ClientStorage'
 import { useDispatch } from 'react-redux'
-import { receiveCurrentUser } from '../state/actions'
+import {
+	receiveCurrentUser,
+	receiveRequestsMadeToMe,
+	receiveRequestsMadeByMe,
+} from '../state/actions'
 import api from '../api'
 
 export function useHandleLogin() {
@@ -12,7 +16,14 @@ export function useHandleLogin() {
 			// set the current user in local storagen
 			setCurrentUser(login)
 			api.getExpertConnections(login).then(requests => {
-				console.log('Connection Requests', requests)
+				const requestsMadeByMe = requests.filter(
+					r => r.type === 'requestsOriginatedByMe',
+				)
+				const requestsMadeToMe = requests.filter(
+					r => r.type === 'requestsReceivedByMe',
+				)
+				dispatch(receiveRequestsMadeByMe(requestsMadeByMe))
+				dispatch(receiveRequestsMadeToMe(requestsMadeToMe))
 			})
 			api.getEmployees().then(employees => {
 				const found = employees.find(e => e.email === login)
