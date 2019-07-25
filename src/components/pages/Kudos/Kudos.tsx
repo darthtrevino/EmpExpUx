b4755761f0f193b771c66077a86f04541ca157e8
Brian Garland <bgarland@bpcs.com>
2019-07-24 18:10:57 -0700

    interface to send kudos and show sent kudos

--- src/components/pages/Kudos/Kudos.tsx
+++ src/components/pages/Kudos/Kudos.tsx
@@ -2,17 +2,33 @@ import React from 'react'
 import { useSelector } from 'react-redux'
 import { State } from '../../../state/reducers'
 import { KudoView } from './KudoView'
+import { KudoByMeView } from './KudoByMeView'
+import { Pivot, PivotItem } from 'office-ui-fabric-react'
 import styles from './Kudos.module.scss'
 
 export const Kudos: React.FC = () => {
-	const kudos = useSelector((state: State) => state.kudos.toMe)
+	const kudosByMe = useSelector((state: State) => state.kudos.byMe)
+	const kudosToMe = useSelector((state: State) => state.kudos.toMe)
 	return (
 		<div className={styles.container}>
 			<div className={styles.gutter} />
 			<div className={styles.content}>
-				{kudos.map(k => (
-					<KudoView kudo={k} />
-				))}
+				<Pivot>
+					<PivotItem headerText="Kudos Suggestions">
+						<div className={styles.pivotContent}>
+							{kudosByMe.map(k => (
+								<KudoByMeView kudo={k} />
+							))}
+						</div>
+					</PivotItem>
+					<PivotItem headerText="Kudos Received">
+						<div className={styles.pivotContent}>
+							{kudosToMe.map(k => (
+								<KudoView kudo={k} />
+							))}
+						</div>
+					</PivotItem>
+				</Pivot>
 			</div>
 			<div className={styles.gutter} />
 		</div>
